a58c324193764582808052c59840aec5d60e6a7b
Evgeniy Shumakov <evgeny@async.local>
2016-02-21 14:26:32 +0300

    Baseline

--- SrcParser/main.cpp
+++ SrcParser/main.cpp
@@ -7,32 +7,104 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+void srcparser(string arg) {
+    
+    string filename;
+    string line;
+    int count;
+    // Cheking if single source file in argument
+    
+    
+    if(arg.substr(arg.length()-4,4)==".cpp") {
+        filename=arg;
+        
+        cout << "Source file specified\n";
+        try {
+            ifstream ifile(filename);
+            if(ifile.is_open()) {
+                cout << "Filename:\t" << filename << "\n";
+                cout << "Included:\n";
+                while(getline(ifile,line)) {
+                    if(line.substr(0,8)=="#include") {
+                        cout << "\t" << line.substr(9,line.length()-9) << "\n";
+                        count++;
+                    }
+                  }
+                if(count>0) {
+                    cout << "Founds:" << count << "\n";
+                }
+                else {
+                    cout << "No include founds\n";
+                }
+                ifile.close();
+            }
+        }
+        catch (exception e) {
+            cout << e.what() << "\n";
+        }
+        
+        exit(0);
+    }
+    if(arg.substr(arg.length()-2,2)==".h") {
+        filename=arg;
+        cout << "Header file specified\n";
+        try {
+            ifstream ifile(filename);
+            if(ifile.is_open()) {
+                cout << "Filename:\t" << filename << "\n";
+                cout << "Included:\n";
+                while(getline(ifile,line)) {
+                    if(line.substr(0,8)=="#include") {
+                        cout << "\t" << line.substr(9,line.length()-9) << "\n";
+                        count++;
+                    }
+                    
+                }
+                if(count>0) {
+                    cout << "Founds:" << count << "\n";
+                }
+                else {
+                    cout << "No include founds\n";
+                }
+                ifile.close();
+            }
+        }
+        catch (exception e) {
+            cout << e.what() << "\n";
+        }
+        
+        exit(0);
+        
+    }
+    
+        cout << "Directory specified\n";
+        cout << arg << "\n";
+    
+        
+}
 
 void argparser(int argc, const char * argv[]) {
     
     // Validation number of arguments
     
     if(argc<1) {
-        std::cout << "Incorrect number of arguments\n";
+        cout << "Incorrect number of arguments\n";
         exit(0);
     }
     
-    
-}
-
-void srcparser(char * argv[]) {
-    // Cheking if single source file in argument
-    if(argv[0]=="source.cpp") {
-        std::cout << "Source file specified\n";
-    }
-    else {
-        std::cout << "Directory specified\n";
-    }
-    
-        
+    srcparser(argv[1]);
 }
 
 int main(int argc, const char * argv[]) {
+    // Test block
+    //std::cout << argv[0] << "\n";
     
     argparser(argc,argv);
     
