284c087153a047c381b7807a766e38ea6e9fab7d
Evgeniy Shumakov <evgeny@async.local>
2016-02-21 11:26:36 +0300

    Initial Commit

---
+++ SrcParser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SrcParser
+//
+//  Created by Evgeniy Shumakov on 21.02.16.
+//  Copyright © 2016 Evgeniy Shumakov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
